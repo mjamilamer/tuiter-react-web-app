f01788dc5705a0f2e0130b4ef6452ab878f0e4c3
Mohammed Amer <99843245+mjamilamer@users.noreply.github.com>
2023-04-02 16:40:00 -0400

    a8 env variable

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'https://tuiter-node-server-app-840r.onrender.com/api/tuits'
 
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`
 
 export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
