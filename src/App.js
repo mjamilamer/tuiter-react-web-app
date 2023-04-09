955e5b4d779b2853dd6f11628927debd99a4d8f2
Admin <mjamilamer@gmail.com>
2023-04-09 15:58:48 -0400

    mongoose

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom"
 import {Routes, Route} from "react-router"
+import mongoose from "mongoose"
+
+mongoose.connect('mongodb://127.0.01:27017/tuiter');
 
 function App() {
     return (
